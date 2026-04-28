6c97563300d04e46e984f12ef4c9530da767844e
KALDYBAKYZY <zhaiylgangulnaz@gmail.com>
2026-04-29 01:03:29 +0500

    qw

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const http = require("http");
+
+// routes
 const authRoutes = require("./routes/auth");
 const stockRoutes = require("./routes/stocks");
 const transactionRoutes = require("./routes/transactions");
+
 const initWebSocket = require("./ws/ws_server");
+
 const app = express();
 
 app.use(express.json());
+
 app.use(
   cors({
     origin: [
@@ -18,28 +23,37 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.options("*", cors());
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/stocks", stockRoutes);
 app.use("/api/transactions", transactionRoutes);
 
 app.get("/", (req, res) => {
-  res.send("API + WS работает");
+  res.send("PEX API + WebSocket is running 🚀");
 });
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB подключен"))
-  .catch((err) => console.log("Mongo error:", err));
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB error:", err));
+
 
 const server = http.createServer(app);
 initWebSocket(server);
 
+
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
-  console.log("Server запущен на порту", PORT);
+  console.log(`Server running on port ${PORT}`);
 });
