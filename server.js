c411390f23cb02623331042e02a9378b8c60e632
KALDYBAKYZY <zhaiylgangulnaz@gmail.com>
2026-04-29 00:33:02 +0500

    fix cors properly

--- server.js
+++ server.js
@@ -1,54 +1,45 @@
 require("dotenv").config();
-const cors = require("cors");
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const http = require("http");
-
-// routes
 const authRoutes = require("./routes/auth");
 const stockRoutes = require("./routes/stocks");
 const transactionRoutes = require("./routes/transactions");
-
-// websocket
 const initWebSocket = require("./ws/ws_server");
-
 const app = express();
-
-// middleware
-app.use(cors());
-app.use(express.json());
 
 app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://frontend-midterm-fullstack.vercel.app"
   ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
-})); 
+}));
 
-// routes
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://frontend-midterm-fullstack.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/stocks", stockRoutes);
 app.use("/api/transactions", transactionRoutes);
 
-// test route
 app.get("/", (req, res) => {
   res.send("API + WS работает 🚀");
 });
-
-// MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB подключен"))
   .catch(err => console.log(err));
-
-// создаём HTTP сервер
 const server = http.createServer(app);
-
-// подключаем WebSocket
 initWebSocket(server);
-
-// запуск
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
